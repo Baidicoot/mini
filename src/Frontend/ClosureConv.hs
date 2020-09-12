fcb80a181e689c97676e452d3169b64aa0d70494
Aidan K. Ewart <aidanprattewart@gmail.com>
2020-09-12 12:31:58 +0100

    closureconv

--- src/Frontend/ClosureConv.hs
+++ src/Frontend/ClosureConv.hs
@@ -7,15 +7,20 @@ import Types.Ident
 import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.State
+import Control.Monad.Identity
 
 import Data.Maybe
 
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 
 type Escaping = Set.Set Identifier
-type Env = Escaping
-type ClosureState = [Name]
-type ClosureConv = ReaderT Env (State ClosureState)
+type Functions = Set.Set Identifier
+-- cache of function -> fvs for known functions
+-- iteration used to handle cycles
+type Freevars = Map.Map Identifier (Set.Set Identifier)
+-- argument capping is done in a separate pass
+type ClosureConv = ReaderT (Escaping, Functions, FreeVars) (State Freevars)
 
 escV :: Value -> Maybe Identifier
 escV = (\case
@@ -23,7 +28,7 @@ escV = (\case
     Label id -> Just id
     _ -> Nothing)
 
-escaping :: CExp -> Set.Set Identifier
+escaping :: CExp -> Escaping
 escaping (App _ vs) = Set.fromList . catMaybes . fmap escV $ vs
 escaping (Fix fns exp) = mappend (escaping exp) $ mconcat . fmap (\(Fun _ _ exp) -> escaping exp) $ fns
 escaping (Record vs _ exp) = mappend (escaping exp) $ Set.fromList . catMaybes . fmap (escV . fst) $ vs
@@ -32,42 +37,33 @@ escaping (Switch _ exps) = mconcat (fmap escaping exps)
 escaping (Primop _ vs _ exps) = mappend (escaping exp) $ Set.fromList . catMaybes . fmap escV $ vs
 escaping _ = mempty
 
-{-
-fresh :: ClosureConv Name
-fresh = do
-    names <- get
-    let (n:ns) = names
-    put ns
-    pure n
+functions :: CExp -> Functions
+functions (Fix fns exp) = mappend (Set.fromList . fmap fst $ fns) (functions exp)
+functions (Record _ _ exp) = functions exp
+functions (Select _ _ _ exp) = functions exp
+functions (Switch _ exps) = mconcat . fmap functions $ exps
+functions (Primop _ _ _ exps) = mconcat . fmap functions $ exps
+functions _ = mempty
 
-ftvId :: Identifier -> ClosureConv Env
-ftvId id = do
-    env <- ask
-    pure $ if id `Set.member` env then Set.singleton id else mempty
+-- only handles CExp that it is expected to meet
+fv :: CExp -> Set.Set Identifier
+fv (App n vs) = Set.fromList . catMaybes . fmap escV $ (n:vs)
+fv (Fix fns exp) = flip Set.difference (Set.fromList . fmap (\(Fun id _ _) -> id) $ fns) $ Set.union (fv exp) $ fmap (\(Fun _ args exp) -> fv exp `Set.disjoint` (Set.fromList args))
+fv (Record vs n exp) = Set.delete n $ (fv exp) `Set.union` (catMaybes . fmap (escV . fst) $ vs)
+fv (Select _ v n exp) = Set.delete (LocalIdentifier n) $ Set.fromList (maybeToList (escV v)) `Set.union` fv exp
+fv (Switch v exps) = mconcat (fmap fv exps) `Set.union` (Set.fromList . maybeToList . escV $ v)
+fv (Primop _ args n exps) = Set.delete (LocalIdentifier n) $ mconcat (fmap fv exps) `Set.union` (Set.fromList . catMaybes . fmap escV $ args)
+fv _ = mempty
 
-ftvV :: Value -> ClosureConv Env
-ftvV (Var id) = ftvId id
-ftvV (Label id) = ftvId id
-ftvV _ = pure mempty
+escapes :: Identifier -> ClosureConv Bool
+escapes id = do
+    (env, _) <- ask
+    pure $ id `Set.member` env
 
-ftvFn :: CFun -> ClosureConv Env
-ftvFn (Fun _ args exp) = do
-    let argenv = Set.fromList $ fmap LocalIdentifier args
-    local (mappend argenv) (ftv exp)
+isKnown :: Identifier -> ClosureConv Bool
+isKnown id = do
+    (_, env) <- ask
+    pure $ id `Set.member` env
 
-ftv :: CExpr -> ClosureConv Env
-ftv (App v vs) = do
-    fvs <- mapM ftvV vs
-    fv <- ftvV v
-    pure $ foldr mappend fv fvs
-ftv (Fix fns exp) = do
-    let names = Set.fromList $ fmap (\(Fun n _ _) -> n) fns
-    local (mappend names) $ do
-        ffv <- mapM ftvFn fns
-        fev <- ftv exp
-        pure $ foldr mappend fev fvs
-ftv (Record vs id cexp) = do
-    fvs <- mapM (ftvV . fst) vs
-    fev <- local (Set.insert id) (ftv cexp)
-    pure $ foldr mappend fev fvs
--}
+alwaysKnown :: Identifier -> ClosureConv Bool
+alwaysKnown = fmap not . escapes
