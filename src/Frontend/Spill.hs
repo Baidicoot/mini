8a0c8e08a097b8b3ea2356f08bf900653f8308e9
Aidan K. Ewart <aidanprattewart@gmail.com>
2020-09-25 12:14:45 +0100

    worked!!!

--- src/Frontend/Spill.hs
+++ src/Frontend/Spill.hs
@@ -79,7 +79,7 @@ of the continuation expressions
 -}
 
 maybeToSet = Set.fromList . maybeToList
-filterFree free rc scope = (Set.filter (`Set.member` free) rc, Map.filterWithKey ((`Set.member` free) . const) scope)
+filterFree (free, _) (rn, rc, scope) = (rn, Set.filter (`Set.member` free) rc, Map.filterWithKey ((`Set.member` free) . const) scope)
 
 unique :: SpillCtx -> Set.Set Name
 unique (_, rc, scope) = Map.keysSet $ Map.filterWithKey ((`Map.notMember` rc) . const) scope
@@ -98,13 +98,52 @@ dropN n ctx@(rn, rc, scope) (fv, depth) =
     let dropping = take n ordered in
         (rn, Map.filterWithKey ((`Set.member` dropping) . const) rc, scope)
 
+keepN :: Int -> Set.Set Name -> Set.Set Name -> VarInfo -> (Set.Set Name, Set.Set Name)
+keepN n uniq duped (fv, depth) =
+    let ordered = order uniq
+    in (take n ordered, drop n ordered)
+    where
+        order = reverse . sortBy (\n -> case Map.lookup n depth of
+            Just x -> -x
+            Nothing -> 0) . Set.toList
+
 -- selectVars takes a spilling context, var order, bound variables, required variables
 -- it returns a selector function for those variables and a new spilling context
 
 -- variable renaming is captured in the returned spilling context
 -- the new context needs to have all the 'arg' variables in scope
-selectVars :: SpillCtx -> VarInfo -> Maybe Name -> Set.Set Name -> Spill (SpillCtx, CExp -> CExp)
-selectVars ctx@(rn, rc, scope) bound args = do
+selectVars :: SpillCtx -> VarInfo -> Set.Set Name -> Set.Set Name -> Spill (SpillCtx, CExp -> CExp)
+selectVars ctx@(rn, rc, scope) info bound args = do
     n <- ask
-    let ndup = n - Set.length (bound `Set.union` args)
-    let needsSpill = Map.length scope < ndup
+    let new = bound `Set.union` args
+    let ndup = n - Set.length new
+    let (keeping, dropping) = keepN ndup (unique ctx) (duplicated ctx) info
+    let needsSpill = Set.length dropping > 0
+    -- split into functions etc here
+    argSelects <- fmap unzip . foldM (\arg (fn, map) -> case Map.lookup arg rc of
+        Just i -> do
+            f <- fresh
+            let binding = Select i (Var $ LocalIdentifier arg) f
+            pure (binding . fn, (arg, f))
+        Nothing -> pure (fn, (arg, arg))) $ Set.toList args
+    (rn', rc', rfn) <- if needsSpill then do
+            let vars = dropping `Set.union` Map.keysSet rc
+            (accesses, locations) = case rn of
+                Just rn -> foldr (\(v, ni) (acs, locs) -> case (Map.lookup v rc, Map.lookup v scope) of
+                    (Just oi, _) ->
+                        let acs' = (Var $ LocalIdentifier rn, SelPath oi (OffPath 0)):acs
+                        in (acs', (v, ni):locs)
+                    (_, Just ov) ->
+                        let acs' = (Var $ LocalIdentifier ov, OffPath 0)
+                        in (acs', (v, ni):locs)
+                    _ -> error "unreachable") (zip (Set.toList vars) [0..])
+                Nothing -> foldr (\(v, ni) (acs, locs) -> case (Map.lookup v scope) of
+                    Just ov ->
+                        let acs' = (Var $ LocalIdentifier ov, OffPath 0)
+                        in (acs', (v, ni):locs)
+                    _ -> error "unreachable") (zip (Set.toList vars) [0..])
+            rn' <- fresh
+            let binding = Record accesses rn'
+            pure (Just rn', Map.fromList locations, binding)
+        else
+            pure (rn, rc, id)
